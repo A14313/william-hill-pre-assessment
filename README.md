9d941f854b35f0cbaaa23a9bea02b96ec8a8e631
Carlo Autor <carloautor@gmail.com>
2023-07-11 22:12:59 +0800

    Initial build
    All screens are supported.
    
    From 320px up to highest possible.
    
    Fully responsive

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # william-hill-pre-assessment
- This is my solution to the pre assessment of William Hill
+
+This is my solution to the pre assessment of William Hill
+
+## My process
+
+### Built with
+
+-   Semantic HTML5 markup
+-   CSS custom properties
+-   Flexbox
+-   CSS Grid
+-   SCSS
+-   Mobile-first workflow
+
+### Features
+
+-   Added some hover transitions and animations
+-   Added mobile layout, from 320px up to the largest possible screen
+-   Fully Responsive Website
+
+#### This is built without any framework ✨.
+
+### Links
+
+-   [Live Site URL](https://a14313.github.io/william-hill-pre-assessment/dist)
+
+## Author
+
+-   [Carlo Autor - Github](https://github.com/A14313)
---
+++ src/js/app.js
@@ -0,0 +1,26 @@
+const url = 'http://feedsapi.safe-installation.com/api/GetJackpotTotalAmount?CurrencyCode=USD&currencySymbol=$&BrandID=0';
+const jackpot = document.querySelector('#jackpot');
+
+function randomCent() {
+	const randomCent = Math.floor(Math.random() * 10) + 1;
+	return randomCent;
+}
+fetch(url)
+	.then((res) => {
+		return res.json();
+	})
+	.then((data) => {
+		let amount = data.entity.totalAmount;
+		let amountConvertedToString = amount.toString();
+
+		// amountConvertedToString.substring(7 + 1)
+		let afterDot = amountConvertedToString.substring(amountConvertedToString.indexOf('.') + 1);
+		let beforeDot = amountConvertedToString.slice(0, amountConvertedToString.indexOf('.'));
+
+		jackpot.innerHTML = `$ ${beforeDot}.${afterDot}`;
+
+		setInterval(() => {
+			afterDot = (parseInt(afterDot) + randomCent()) % 100;
+			jackpot.innerHTML = `$ ${beforeDot}.${afterDot}`;
+		}, 1000);
+	});
